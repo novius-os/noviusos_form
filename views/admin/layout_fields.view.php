b57e45fc7899bbd7fba70db02297225fb5ee85b9
esperat <esperat@novius.fr>
2012-11-22 12:25:52 +0100

    Added answers collected warning.

--- views/admin/layout_fields.view.php
+++ views/admin/layout_fields.view.php
@@ -1,5 +1,10 @@
 <link rel="stylesheet" href="static/apps/noviusos_form/css/admin.css" />
 
+<?php
+if (!$item->is_new() && count($item->answers) > 0) {
+    echo \View::forge('noviusos_form::admin/warning_answers_collected', $view_params, false);
+}
+?>
 
 <div id="<?= $uniqid = uniqid('container_') ?>">
 
