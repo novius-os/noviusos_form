7eabb701b4ba1ed25710c526ec25c3433b41c9b2
Gilles FELIX <felix@novius.com>
2012-12-05 17:32:22 +0100

    Change order of answer's actions

--- config/common/answer.config.php
+++ config/common/answer.config.php
@@ -16,24 +16,30 @@ return array(
         ),
     ),
     'actions' => array(
-        'Nos\Form\Model_Answer.edit' => false,
-        'Nos\Form\Model_Answer.add' => false,
-        'Nos\Form\Model_Answer.visualize' => array(
-            'label' => __('Visualize'),
-            'name' => 'visualize',
-            'iconClasses' => 'nos-icon16 nos-icon16-eye',
-            'primary' => true,
-            'context' => array(
-                'list' => true,
-            ),
-            'action' => array(
-                'action' => 'nosTabs',
-                'tab' => array(
-                    'url' => 'admin/noviusos_form/answer/visualize/{{_id}}',
-                    'label' => __('Answer'),
-                    'iconUrl' => 'static/apps/noviusos_form/img/icons/form-16.png',
+        'list' => array(
+            'Nos\Form\Model_Answer.edit' => false,
+            'Nos\Form\Model_Answer.add' => false,
+            'Nos\Form\Model_Answer.visualize' => array(
+                'label' => __('Visualize'),
+                'name' => 'visualize',
+                'iconClasses' => 'nos-icon16 nos-icon16-eye',
+                'primary' => true,
+                'context' => array(
+                    'list' => true,
+                ),
+                'action' => array(
+                    'action' => 'nosTabs',
+                    'tab' => array(
+                        'url' => 'admin/noviusos_form/answer/visualize/{{_id}}',
+                        'label' => __('Answer'),
+                        'iconUrl' => 'static/apps/noviusos_form/img/icons/form-16.png',
+                    ),
                 ),
             ),
+        ),
+        'order' => array(
+            'Nos\Form\Model_Answer.visualize',
+            'Nos\Form\Model_Answer.delete',
         ),
     ),
 );
