2dff24bcaa101c4ed555e457d5da082ae2d5dadd
Julian Esperat <esperat@novius.fr>
2013-04-16 19:03:01 +0200

    "Adding lang config files"

---
+++ lang/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'front' => array(
+        '^views/foundation.view.php',
+        '^classes/controller/front',
+    ),
+    // false == default dict
+    'common' => false,
+);
