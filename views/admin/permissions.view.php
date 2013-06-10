e002bb06c489468199d84ccd704bbcc0a344b657
Julian Esperat <esperat@novius.fr>
2013-06-10 17:17:21 +0200

    Permissions uniformisation

--- views/admin/permissions.view.php
+++ views/admin/permissions.view.php
@@ -3,14 +3,14 @@
 ?>
 <p>
     <label>
-        <input type="radio" name="perm[noviusos_form::all][]" value="2_write" <?= $role->checkPermissionExistsOrEmpty('noviusos_form::all', '2_write') ? 'checked' : '' ?> />
+        <input type="radio" name="perm[noviusos_form::all][]" value="2_write" <?= (int) $role->getPermissionValue('noviusos_form::all', 2) == 2 ? 'checked' : '' ?> />
         <?= __('Can add, edit and delete forms and answers') ?>
     </label>
 </p>
 
 <p>
     <label>
-        <input type="radio" name="perm[noviusos_form::all][]" value="1_read" <?= $role->checkPermissionExists('noviusos_form::all', '1_read') ? 'checked' : '' ?> />
+        <input type="radio" name="perm[noviusos_form::all][]" value="1_read" <?= (int) $role->getPermissionValue('noviusos_form::all') == 1 ? 'checked' : '' ?> />
         <?= __('Can visualise answers only') ?>
     </label>
 </p>
