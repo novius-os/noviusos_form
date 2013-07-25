ec7202102e6fe1f3541700b09b819485239e01db
Gilles FELIX <felix@novius.com>
2013-07-25 17:18:27 +0200

    Permissions : checked first radio by default

--- views/admin/permissions.view.php
+++ views/admin/permissions.view.php
@@ -1,16 +1,26 @@
 <?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
 
+$all = $role->getPermissionValue('noviusos_form::all', 2);
+$all = $all === false ? 2 : $all;
 ?>
 <p>
     <label>
-        <input type="radio" name="perm[noviusos_form::all][]" value="2_write" <?= (int) $role->getPermissionValue('noviusos_form::all', 2) == 2 ? 'checked' : '' ?> />
+        <input type="radio" name="perm[noviusos_form::all][]" value="2_write" <?= $all == 2 ? 'checked' : '' ?> />
         <?= __('Can add, edit and delete forms and answers') ?>
     </label>
 </p>
 
 <p>
     <label>
-        <input type="radio" name="perm[noviusos_form::all][]" value="1_read" <?= (int) $role->getPermissionValue('noviusos_form::all') == 1 ? 'checked' : '' ?> />
+        <input type="radio" name="perm[noviusos_form::all][]" value="1_read" <?= $all == 1 ? 'checked' : '' ?> />
         <?= __('Can visualise answers only') ?>
     </label>
 </p>
