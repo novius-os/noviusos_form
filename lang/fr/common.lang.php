228292e1f273fd6e35ce90949f2079ac4f993ef7
Gilles FELIX <felix@novius.com>
2014-06-30 15:00:19 +0200

    "Update french translation"

--- lang/fr/common.lang.php
+++ lang/fr/common.lang.php
@@ -1,180 +1,56 @@
 <?php
 
-// Generated on 03/12/2013 16:22:23
+// Generated on 30/06/2014 14:54:18
 
-// 152 out of 152 messages are translated (100%).
-// 634 out of 634 words are translated (100%).
+// 154 out of 154 messages are translated (100%).
+// 591 out of 591 words are translated (100%).
 
 return array(
-    #: classes/controller/admin/answer/appdesk.ctrl.php:30
-    #: config/common/form.config.php:121
-    'Answers to ‘{{title}}’' => 'Réponses à «&nbsp;{{title}}&nbsp;»',
+    #: config/controller/admin/enhancer.config.php:42
+    'Pick a form:' => 'Choisir un formulaire:',
 
-    #: classes/controller/admin/answer/appdesk.ctrl.php:36
-    'Export (spreadsheet)' => 'Exporter (tableur)',
+    #: config/controller/admin/enhancer.config.php:49
+    'No form selected' => 'Aucun formulaire sélectionné',
 
-    #: classes/controller/admin/answer.ctrl.php:33
-    #: views/admin/answer.view.php:25
-    'Answer to ‘{{title}}’' => 'Réponse à «&nbsp;{{title}}&nbsp;»',
+    #: config/controller/admin/enhancer.config.php:50
+    'Pick a form' => 'Choisir un formulaire',
 
-    #: classes/controller/admin/answer.ctrl.php:110
-    'No file attached.' => 'Aucun fichier joint.',
+    #: config/controller/admin/enhancer.config.php:51
+    'Pick another form' => 'Choisir un autre formulaire',
 
-    #: classes/controller/admin/form.ctrl.php:187
-    #: classes/controller/admin/form.ctrl.php:202
-    'Page break' => 'Saut de page',
+    #: config/controller/admin/enhancer.config.php:52
+    'Un-select this form' => 'Dé-sélectionner ce formulaire',
 
-    #: classes/controller/admin/enhancer.ctrl.php:54
-    #: config/controller/admin/enhancer.config.php:76
+    #: config/controller/admin/enhancer.config.php:60
+    'Label position:' => 'Position des libellés&nbsp;:',
+
+    #: config/controller/admin/enhancer.config.php:64
+    'Top aligned' => 'Au-dessus du champ',
+
+    #: config/controller/admin/enhancer.config.php:65
+    'Left aligned' => 'À gauche du champ, aligné à gauche',
+
+    #: config/controller/admin/enhancer.config.php:66
+    'Right aligned' => 'À gauche du champ, aligné à droite',
+
+    #: config/controller/admin/enhancer.config.php:67
+    'In the field (placeholder), for text fields only' => 'Dans le champ (placeholder), pour les champs textes uniquement',
+
+    #: config/controller/admin/enhancer.config.php:73
+    'Once the user submitted the form:' => 'Une fois que l’utilisateur a envoyé le formulaire&nbsp;:',
+
+    #: config/controller/admin/enhancer.config.php:78
+    'Display a message' => 'Afficher un message',
+
+    #: config/controller/admin/enhancer.config.php:79
+    'Redirect to a page' => 'Rediriger vers une page',
+
+    #: config/controller/admin/enhancer.config.php:88
+    #: classes/controller/admin/enhancer.ctrl.php:31
     'Thank you. Your answer has been sent.' => 'Merci. Votre réponse a été envoyée.',
 
-    #: views/enhancer/blank_slate.view.php:16
-    'How frustrating, you have no form to insert. But let’s not worry, shall we? Here is how it works:' => 'Voilà qui est frustrant, vous n’avez aucun formulaire à ajouter. Mais, pas de panique, il vous faut simplement&nbsp;:',
-
-    #: views/enhancer/blank_slate.view.php:19
-    '<ul><li><a>Add your first form</a> (a new tab will open).</li>
-<li>Once you’re done (it won’t take long), come back to this tab to insert your shiny new form.</li></ul>' => '<ul><li><a>Ajoutez votre premier formulaire</a> (s’ouvre dans un nouvel onglet).</li>
-<li>Une fois que vous aurez terminé (ça ne sera pas long), revenez à cet onglet pour insérer votre beau formulaire tout neuf.</li></ul>',
-
-    #: views/enhancer/blank_slate.view.php:27
-    'No forms are available in {{context}}. Go ahead, <a>add your first form in this language</a>.' => 'Aucun formulaire n’est disponible en {{context}}. Allez-y, <a>ajoutez votre premier formulaire dans cette langue</a>.',
-
-    #: views/enhancer/blank_slate.view.php:29
-    'No forms are available in {{context}}. Go ahead, <a>add your first form in this context</a>.' => 'Aucun formulaire n’est disponible en {{context}}. Allez-y, <a>ajoutez votre premier formulaire dans ce contexte</a>.',
-
-    #: views/email.view.php:31
-    'Message sent by:' => 'Message envoyé par :',
-
-    #: views/admin/warning_answers_collected.view.php:26
-    'Modifying the form may alter the collected data.' => 'Le modifier pourrait supprimer des données collectées.',
-
-    #: views/admin/warning_answers_collected.view.php:29
-    'You’re right, take me to the answers' => 'C’est vrai, allons voir les réponses',
-
-    #: views/admin/warning_answers_collected.view.php:31
-    'or' => 'ou',
-
-    #: views/admin/warning_answers_collected.view.php:31
-    'Don’t worry, I know what I’m doing' => 'Ne vous inquiétez pas, je sais ce que je fais',
-
-    #: views/admin/warning_answers_collected.view.php:49
-    'Answers to this form have already been received' => 'Des réponses à ce formulaire ont déjà été reçues',
-
-    #: views/admin/layout_fields.view.php:28
-    'You have a problem here: Your Novius OS is not set up to send emails. You’ll have to ask your developer to set it up for you.' => 'Il y a un souci&nbsp;: Votre Novius OS n’est pas configuré pour envoyer des emails. Demandez à votre développeur de le configurer.',
-
-    #: views/admin/layout_fields.view.php:47
-    #: views/admin/layout_fields.view.php:92
-    'Add a field' => 'Ajouter un champ',
-
-    #: views/admin/layout_fields.view.php:53
-    'Standard fields' => 'Champs standards',
-
-    #: views/admin/layout_fields.view.php:54
-    'Special fields' => 'Champs spéciaux',
-
-    #: views/admin/layout_fields.view.php:93
-    'Add a page break' => 'Ajouter un saut de page',
-
-    #: views/admin/layout_fields.view.php:96
-    'Help us prevent spam: How much is 3 plus 8?' => 'Aidez-nous à lutter contre le spam&nbsp;: Combien font 3 plus 8&nbsp;?',
-
-    #: views/admin/layout_fields.view.php:103
-    'Delete' => 'Supprimer',
-
-    #: views/admin/layout_fields.view.php:132
-    'Form submission' => 'Envoi du formulaire',
-
-    #: views/admin/layout_fields.view.php:172
-    'Are you sure?' => 'Êtes-vous sur(e)&nbsp;?',
-
-    #: views/admin/permissions.view.php:15
-    'Can add, edit and delete forms and answers' => 'Peut ajouter, modifier et supprimer des formulaires et réponses',
-
-    #: views/admin/permissions.view.php:22
-    'Can visualise answers only' => 'Peut uniquement visualiser les réponses',
-
-    #: views/admin/popup_delete.view.php:24
-    'This form has already received <strong>one answer</strong>.' => array(
-        0 => 'Ce formulaire a déjà reçu <strong>une réponse</strong>.',
-        1 => 'Ce formulaire a déjà reçu <strong>{{count}} réponses</strong>.',
-    ),
-
-    #: views/admin/popup_delete.view.php:33
-    'Yes, I want to delete this form and the {{count}} answers received.' => 'Oui, je veux supprimer ce formulaire et les {{count}} réponses reçues.',
-
-    #: views/admin/warning_not_published.view.php:18
-    'Not published' => 'Non publié',
-
-    #: views/admin/warning_not_published.view.php:19
-    'To publish this form, add it to a page, a blog post or any other text editor.' => 'Pour publier ce formulaire, ajoutez-le à une page, un billet de blog ou tout autre éditeur de texte.',
-
-    #: views/admin/warning_not_published.view.php:22
-    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '<p>La plupart des éditeurs de texte de Novius OS (aussi appelés éditeurs WYSIWYG) ont ce bouton {{preview}} dans la barre d’outils.</p><p>Cliquez pour accéder à la liste des applications permettant d’enrichir vos contenus.</p>',
-
-    #: views/admin/warning_not_published.view.php:23
-    'Applications' => 'Applications',
-
-    #: views/admin/answer.view.php:15
-    'Cancel' => 'Annuler',
-
-    #: views/admin/answer.view.php:26
-    'Received on {{date}}' => 'Reçue le {{date}}',
-
-    #: views/admin/answer.view.php:33
-    'Question' => 'Question',
-
-    #: views/admin/answer.view.php:34
-    'Answer' => 'Réponse',
-
-    #: views/admin/answer.view.php:55
-    'Page {{number}}' => 'Page {{number}}',
-
-    #: views/admin/answer.view.php:102
-    #: views/admin/answer.view.php:112
-    'Add to Media Centre' => 'Ajouter à la Médiathèque',
-
-    #: config/permissions.config.php:11
-    'Permissions for this application' => 'Droits d’accès pour cette application',
-
-    #: config/controller/admin/answer/inspector/date.config.php:18
-    'Answer date' => 'Date de la réponse',
-
-    #: config/controller/admin/answer/appdesk.config.php:25
-    'Received on:' => 'Reçue le&nbsp;:',
-
-    #. Note to translator: 'Preview' here is a label, not an action
-    #: config/controller/admin/answer/appdesk.config.php:31
-    'Preview' => 'Prévisualisation',
-
-    #: config/controller/admin/answer/appdesk.config.php:32
-    'Click on an answer to preview it.' => 'Cliquez sur une réponse pour la prévisualiser.',
-
-    #: config/controller/admin/answer/appdesk.config.php:39
-    'answer' => 'réponse',
-
-    #: config/controller/admin/answer/appdesk.config.php:40
-    'answers' => 'réponses',
-
-    #: config/controller/admin/answer/appdesk.config.php:42
-    #: config/common/answer.config.php:57
-    '1 answer' => array(
-        0 => '1 réponse',
-        1 => '{{count}} réponses',
-    ),
-
-    #: config/controller/admin/answer/appdesk.config.php:46
-    'Showing 1 answer out of {{y}}' => array(
-        0 => '1 réponse sur {{y}} affichée',
-        1 => '{{x}} réponses sur {{y}} affichées',
-    ),
-
-    #: config/controller/admin/answer/appdesk.config.php:49
-    'No answers' => 'Pas de réponse',
-
-    #. Note to translator: This is the action that clears the 'Search' field
-    #: config/controller/admin/answer/appdesk.config.php:51
-    'Show all answers' => 'Afficher toutes les réponses',
+    #: config/controller/admin/enhancer.config.php:112
+    'Form' => 'Formulaire',
 
     #: config/controller/admin/form.config.php:18
     #: config/common/form.config.php:82
@@ -405,35 +281,44 @@ M.',
     #: config/controller/admin/form.config.php:546
     'CSS classes:' => 'Classes CSS&nbsp;:',
 
-    #: config/controller/admin/enhancer.config.php:42
-    'Select a form:' => 'Sélectionner un formulaire&nbsp;:',
+    #: config/controller/admin/answer/inspector/date.config.php:18
+    'Answer date' => 'Date de la réponse',
 
-    #: config/controller/admin/enhancer.config.php:48
-    'Label position:' => 'Position des libellés&nbsp;:',
+    #: config/controller/admin/answer/appdesk.config.php:25
+    'Received on:' => 'Reçue le&nbsp;:',
 
-    #: config/controller/admin/enhancer.config.php:52
-    'Top aligned' => 'Au-dessus du champ',
+    #. Note to translator: 'Preview' here is a label, not an action
+    #: config/controller/admin/answer/appdesk.config.php:31
+    'Preview' => 'Prévisualisation',
 
-    #: config/controller/admin/enhancer.config.php:53
-    'Left aligned' => 'À gauche du champ, aligné à gauche',
+    #: config/controller/admin/answer/appdesk.config.php:32
+    'Click on an answer to preview it.' => 'Cliquez sur une réponse pour la prévisualiser.',
 
-    #: config/controller/admin/enhancer.config.php:54
-    'Right aligned' => 'À gauche du champ, aligné à droite',
+    #: config/controller/admin/answer/appdesk.config.php:39
+    'answer' => 'réponse',
 
-    #: config/controller/admin/enhancer.config.php:55
-    'In the field (placeholder), for text fields only' => 'Dans le champ (placeholder), pour les champs textes uniquement',
+    #: config/controller/admin/answer/appdesk.config.php:40
+    'answers' => 'réponses',
 
-    #: config/controller/admin/enhancer.config.php:61
-    'Once the user submitted the form:' => 'Une fois que l’utilisateur a envoyé le formulaire&nbsp;:',
+    #: config/controller/admin/answer/appdesk.config.php:42
+    #: config/common/answer.config.php:57
+    '1 answer' => array(
+        0 => '1 réponse',
+        1 => '{{count}} réponses',
+    ),
 
-    #: config/controller/admin/enhancer.config.php:66
-    'Display a message' => 'Afficher un message',
+    #: config/controller/admin/answer/appdesk.config.php:46
+    'Showing 1 answer out of {{y}}' => array(
+        0 => '1 réponse sur {{y}} affichée',
+        1 => '{{x}} réponses sur {{y}} affichées',
+    ),
 
-    #: config/controller/admin/enhancer.config.php:67
-    'Redirect to a page' => 'Rediriger vers une page',
+    #: config/controller/admin/answer/appdesk.config.php:49
+    'No answers' => 'Pas de réponse',
 
-    #: config/controller/admin/enhancer.config.php:100
-    'Form' => 'Formulaire',
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/answer/appdesk.config.php:51
+    'Show all answers' => 'Afficher toutes les réponses',
 
     #: config/controller/admin/appdesk.config.php:17
     'form' => 'formulaire',
@@ -460,9 +345,6 @@ M.',
     #. Note to translator: This is the action that clears the 'Search' field
     #: config/controller/admin/appdesk.config.php:29
     'Show all forms' => 'Afficher tous les formulaires',
-
-    #: config/model/answer.config.php:13
-    'Answer received on {{date}}' => 'Réponse reçue le {{date}}',
 
     #: config/common/form.config.php:28
     #: config/common/form.config.php:111
@@ -494,6 +376,10 @@ M.',
 
     #: config/common/form.config.php:73
     'We cannot delete this form as the number of answers you’ve entered is wrong. Please amend it.' => 'Nous ne pouvons supprimer ce formulaire car le nombre de réponses indiqué est faux. Merci de le corriger.',
+
+    #: config/common/form.config.php:121
+    #: classes/controller/admin/answer/appdesk.ctrl.php:30
+    'Answers to ‘{{title}}’' => 'Réponses à «&nbsp;{{title}}&nbsp;»',
 
     #: config/common/form.config.php:135
     #: config/common/form.config.php:159
@@ -531,5 +417,123 @@ M.',
 
     #: config/common/answer.config.php:76
     'Answer to ’{{title}}’' => 'Réponse à «&nbsp;{{title}}&nbsp;»',
+
+    #: config/permissions.config.php:11
+    'Permissions for this application' => 'Droits d’accès pour cette application',
+
+    #: config/model/answer.config.php:13
+    'Answer received on {{date}}' => 'Réponse reçue le {{date}}',
+
+    #: views/captcha.view.php:22
+    'Help us prevent spam: How much is {{number_1}} plus {{number_2}}?' => 'Aidez-nous à lutter contre le spam: Combien font {{number_1}} plus {{number_2}}&nbsp;?',
+
+    #: views/captcha.view.php:35
+    'You have not passed the spam test. Please try again.' => 'Vous n’avez pas passé le test contre le spam. Merci de ré-essayer.',
+
+    #: views/admin/warning_answers_collected.view.php:26
+    'Modifying the form may alter the collected data.' => 'Le modifier pourrait supprimer des données collectées.',
+
+    #: views/admin/warning_answers_collected.view.php:29
+    'You’re right, take me to the answers' => 'C’est vrai, allons voir les réponses',
+
+    #: views/admin/warning_answers_collected.view.php:31
+    'or' => 'ou',
+
+    #: views/admin/warning_answers_collected.view.php:31
+    'Don’t worry, I know what I’m doing' => 'Ne vous inquiétez pas, je sais ce que je fais',
+
+    #: views/admin/warning_answers_collected.view.php:49
+    'Answers to this form have already been received' => 'Des réponses à ce formulaire ont déjà été reçues',
+
+    #: views/admin/popup_delete.view.php:24
+    'This form has already received <strong>one answer</strong>.' => array(
+        0 => 'Ce formulaire a déjà reçu <strong>une réponse</strong>.',
+        1 => 'Ce formulaire a déjà reçu <strong>{{count}} réponses</strong>.',
+    ),
+
+    #: views/admin/popup_delete.view.php:33
+    'Yes, I want to delete this form and the {{count}} answers received.' => 'Oui, je veux supprimer ce formulaire et les {{count}} réponses reçues.',
+
+    #: views/admin/warning_not_published.view.php:18
+    'Not published' => 'Non publié',
+
+    #: views/admin/warning_not_published.view.php:19
+    'To publish this form, add it to a page, a blog post or any other text editor.' => 'Pour publier ce formulaire, ajoutez-le à une page, un billet de blog ou tout autre éditeur de texte.',
+
+    #: views/admin/warning_not_published.view.php:22
+    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '<p>La plupart des éditeurs de texte de Novius OS (aussi appelés éditeurs WYSIWYG) ont ce bouton {{preview}} dans la barre d’outils.</p><p>Cliquez pour accéder à la liste des applications permettant d’enrichir vos contenus.</p>',
+
+    #: views/admin/warning_not_published.view.php:23
+    'Applications' => 'Applications',
+
+    #: views/admin/permissions.view.php:15
+    'Can add, edit and delete forms and answers' => 'Peut ajouter, modifier et supprimer des formulaires et réponses',
+
+    #: views/admin/permissions.view.php:22
+    'Can visualise answers only' => 'Peut uniquement visualiser les réponses',
+
+    #: views/admin/layout_fields.view.php:28
+    'You have a problem here: Your Novius OS is not set up to send emails. You’ll have to ask your developer to set it up for you.' => 'Il y a un souci&nbsp;: Votre Novius OS n’est pas configuré pour envoyer des emails. Demandez à votre développeur de le configurer.',
+
+    #: views/admin/layout_fields.view.php:47
+    #: views/admin/layout_fields.view.php:92
+    'Add a field' => 'Ajouter un champ',
+
+    #: views/admin/layout_fields.view.php:53
+    'Standard fields' => 'Champs standards',
+
+    #: views/admin/layout_fields.view.php:54
+    'Special fields' => 'Champs spéciaux',
+
+    #: views/admin/layout_fields.view.php:93
+    'Add a page break' => 'Ajouter un saut de page',
+
+    #: views/admin/layout_fields.view.php:96
+    'Help us prevent spam: How much is 3 plus 8?' => 'Aidez-nous à lutter contre le spam&nbsp;: Combien font 3 plus 8&nbsp;?',
+
+    #: views/admin/layout_fields.view.php:103
+    'Delete' => 'Supprimer',
+
+    #: views/admin/layout_fields.view.php:132
+    'Form submission' => 'Envoi du formulaire',
+
+    #: views/admin/layout_fields.view.php:172
+    'Are you sure?' => 'Êtes-vous sur(e)&nbsp;?',
+
+    #: views/admin/answer.view.php:15
+    'Cancel' => 'Annuler',
+
+    #: views/admin/answer.view.php:25
+    #: classes/controller/admin/answer.ctrl.php:33
+    'Answer to ‘{{title}}’' => 'Réponse à «&nbsp;{{title}}&nbsp;»',
+
+    #: views/admin/answer.view.php:26
+    'Received on {{date}}' => 'Reçue le {{date}}',
+
+    #: views/admin/answer.view.php:33
+    'Question' => 'Question',
+
+    #: views/admin/answer.view.php:34
+    'Answer' => 'Réponse',
+
+    #: views/admin/answer.view.php:55
+    'Page {{number}}' => 'Page {{number}}',
+
+    #: views/admin/answer.view.php:88
+    #: views/admin/answer.view.php:98
+    'Add to Media Centre' => 'Ajouter à la Médiathèque',
+
+    #: views/email.view.php:31
+    'Message sent by:' => 'Message envoyé par :',
+
+    #: classes/controller/admin/form.ctrl.php:187
+    #: classes/controller/admin/form.ctrl.php:202
+    'Page break' => 'Saut de page',
+
+    #: classes/controller/admin/answer.ctrl.php:110
+    'No file attached.' => 'Aucun fichier joint.',
+
+    #: classes/controller/admin/answer/appdesk.ctrl.php:36
+    'Export (spreadsheet)' => 'Exporter (tableur)',
 
 );
--- lang/fr/front.lang.php
+++ lang/fr/front.lang.php
@@ -1,49 +1,45 @@
 <?php
 
-// Generated on 03/12/2013 16:22:23
+// Generated on 30/06/2014 14:54:18
 
-// 13 out of 13 messages are translated (100%).
-// 76 out of 76 words are translated (100%).
+// 12 out of 12 messages are translated (100%).
+// 66 out of 66 words are translated (100%).
 
 return array(
-    #: classes/controller/front.ctrl.php:67
-    'Thank you. Your answer has been sent.' => 'Merci. Votre réponse a été envoyée.',
-
-    #: classes/controller/front.ctrl.php:126
-    'Help us prevent spam: How much is {{number_1}} plus {{number_2}}?' => 'Aidez-nous à lutter contre le spam: Combien font {{number_1}} plus {{number_2}}&nbsp;?',
-
-    #: classes/controller/front.ctrl.php:175
-    #: classes/controller/front.ctrl.php:486
-    'You have not passed the spam test. Please try again.' => 'Vous n’avez pas passé le test contre le spam. Merci de ré-essayer.',
-
-    #: classes/controller/front.ctrl.php:424
-    '{{label}}: Please select a file for this field.' => '{{label}}&nbsp;: Merci de sélectionner un fichier pour ce champ.',
-
-    #: classes/controller/front.ctrl.php:450
-    '{{label}}: Please enter a value for this field.' => '{{label}}&nbsp;: Merci de remplir ce champ.',
-
-    #: classes/controller/front.ctrl.php:454
-    '{{label}}: ‘{{value}}’ is not a valid email.' => '{{label}}&nbsp;: {{value}} n’est pas un email valide.',
-
-    #: classes/controller/front.ctrl.php:457
-    '{{label}}: ‘{{value}}’ is not a valid number.' => '{{label}}&nbsp;: {{value}} n’est pas un nombre.',
-
-    #: classes/controller/front.ctrl.php:465
-    '{{label}}: ‘{{value}}’ is not a valid date.' => '{{label}}&nbsp;: {{value}} n’est pas une date valide.',
-
-    #: classes/controller/front.ctrl.php:560
-    '{{form}}: New answer' => '{{form}}&nbsp;: Nouvelle réponse',
-
-    #: views/foundation.view.php:229
+    #: views/foundation.view.php:149
     '{{previous}}{{pagination}}{{next}}' => '',
 
-    #: views/foundation.view.php:238
+    #: views/foundation.view.php:158
     'Previous page' => 'Page précédente',
 
-    #: views/foundation.view.php:242
+    #: views/foundation.view.php:162
     'Next page' => 'Page suivante',
 
-    #: views/foundation.view.php:249
+    #: views/foundation.view.php:169
     '{{current}} out of {{total}}' => '{{current}} sur {{total}}',
+
+    #: classes/controller/front.ctrl.php:83
+    'Thank you. Your answer has been sent.' => 'Merci. Votre réponse a été envoyée.',
+
+    #: classes/controller/front.ctrl.php:444
+    '{{label}}: Please select a file for this field.' => '{{label}}&nbsp;: Merci de sélectionner un fichier pour ce champ.',
+
+    #: classes/controller/front.ctrl.php:470
+    '{{label}}: Please enter a value for this field.' => '{{label}}&nbsp;: Merci de remplir ce champ.',
+
+    #: classes/controller/front.ctrl.php:474
+    '{{label}}: ‘{{value}}’ is not a valid email.' => '{{label}}&nbsp;: {{value}} n’est pas un email valide.',
+
+    #: classes/controller/front.ctrl.php:477
+    '{{label}}: ‘{{value}}’ is not a valid number.' => '{{label}}&nbsp;: {{value}} n’est pas un nombre.',
+
+    #: classes/controller/front.ctrl.php:485
+    '{{label}}: ‘{{value}}’ is not a valid date.' => '{{label}}&nbsp;: {{value}} n’est pas une date valide.',
+
+    #: classes/controller/front.ctrl.php:512
+    'You have not passed the spam test. Please try again.' => 'Vous n’avez pas passé le test contre le spam. Merci de ré-essayer.',
+
+    #: classes/controller/front.ctrl.php:587
+    '{{form}}: New answer' => '{{form}}&nbsp;: Nouvelle réponse',
 
 );
