b5a77975f13a0d7d32668787c8399d5f41663134
Gilles FELIX <felix@novius.com>
2014-07-07 14:48:10 +0200

    "Add translation files for polish, portuguese and portuguese brazil"

---
+++ lang/pt/common.lang.php
@@ -0,0 +1,536 @@
+<?php
+
+// Generated on 07/07/2014 14:44:21
+
+// 147 out of 154 messages are translated (95%).
+// 560 out of 591 words are translated (95%).
+
+return array(
+    #: config/controller/admin/enhancer.config.php:42
+    'Pick a form:' => '',
+
+    #: config/controller/admin/enhancer.config.php:49
+    'No form selected' => '',
+
+    #: config/controller/admin/enhancer.config.php:50
+    'Pick a form' => '',
+
+    #: config/controller/admin/enhancer.config.php:51
+    'Pick another form' => '',
+
+    #: config/controller/admin/enhancer.config.php:52
+    'Un-select this form' => '',
+
+    #: config/controller/admin/enhancer.config.php:60
+    'Label position:' => '',
+
+    #: config/controller/admin/enhancer.config.php:64
+    'Top aligned' => '',
+
+    #: config/controller/admin/enhancer.config.php:65
+    'Left aligned' => '',
+
+    #: config/controller/admin/enhancer.config.php:66
+    'Right aligned' => '',
+
+    #: config/controller/admin/enhancer.config.php:67
+    'In the field (placeholder), for text fields only' => '',
+
+    #: config/controller/admin/enhancer.config.php:73
+    'Once the user submitted the form:' => '',
+
+    #: config/controller/admin/enhancer.config.php:78
+    'Display a message' => '',
+
+    #: config/controller/admin/enhancer.config.php:79
+    'Redirect to a page' => '',
+
+    #: config/controller/admin/enhancer.config.php:88
+    #: classes/controller/admin/enhancer.ctrl.php:31
+    'Thank you. Your answer has been sent.' => '',
+
+    #: config/controller/admin/enhancer.config.php:112
+    'Form' => '',
+
+    #: config/controller/admin/form.config.php:18
+    #: config/common/form.config.php:82
+    'Add a form' => '',
+
+    #: config/controller/admin/form.config.php:47
+    #: config/common/form.config.php:25
+    'Title' => '',
+
+    #: config/controller/admin/form.config.php:57
+    'Virtual name:' => '',
+
+    #: config/controller/admin/form.config.php:69
+    'Captcha protected' => '',
+
+    #: config/controller/admin/form.config.php:77
+    'Submit button’s label:' => '',
+
+    #: config/controller/admin/form.config.php:80
+    'I’m the submit button, click to edit me' => '',
+
+    #: config/controller/admin/form.config.php:84
+    'For every new answer, an email notification is sent to:' => '',
+
+    #. Note to translator: This is a placeholder, i.e. a field’s label shown within the field
+    #: config/controller/admin/form.config.php:86
+    #: config/controller/admin/form.config.php:89
+    'One email per line' => '',
+
+    #: config/controller/admin/form.config.php:102
+    #: config/controller/admin/form.config.php:246
+    'Firstname:' => '',
+
+    #: config/controller/admin/form.config.php:106
+    'Lastname:' => '',
+
+    #: config/controller/admin/form.config.php:110
+    'Email address:' => '',
+
+    #: config/controller/admin/form.config.php:118
+    #: config/controller/admin/form.config.php:389
+    'Single line text' => '',
+
+    #: config/controller/admin/form.config.php:130
+    #: config/controller/admin/form.config.php:390
+    'Paragraph text' => '',
+
+    #: config/controller/admin/form.config.php:142
+    #: config/controller/admin/form.config.php:391
+    'Multiple choice (checkboxes)' => '',
+
+    #: config/controller/admin/form.config.php:148
+    #: config/controller/admin/form.config.php:161
+    'First option
+Second option' => '',
+
+    #: config/controller/admin/form.config.php:155
+    #: config/controller/admin/form.config.php:392
+    'Unique choice (drop-down list)' => '',
+
+    #: config/controller/admin/form.config.php:168
+    #: config/controller/admin/form.config.php:393
+    'Unique choice (radio buttons)' => '',
+
+    #: config/controller/admin/form.config.php:174
+    'First choice
+Second choice' => '',
+
+    #: config/controller/admin/form.config.php:181
+    #: config/controller/admin/form.config.php:394
+    'File' => '',
+
+    #: config/controller/admin/form.config.php:187
+    'I’m the label of a file input, click to edit me:' => '',
+
+    #: config/controller/admin/form.config.php:196
+    #: config/controller/admin/form.config.php:395
+    'Email address' => '',
+
+    #: config/controller/admin/form.config.php:202
+    'Your email address:' => '',
+
+    #: config/controller/admin/form.config.php:209
+    #: config/controller/admin/form.config.php:396
+    'Number' => '',
+
+    #: config/controller/admin/form.config.php:215
+    'Enter a number:' => '',
+
+    #: config/controller/admin/form.config.php:222
+    #: config/controller/admin/form.config.php:397
+    'Date' => '',
+
+    #: config/controller/admin/form.config.php:228
+    'Pick a date:' => '',
+
+    #: config/controller/admin/form.config.php:235
+    'Full name' => '',
+
+    #: config/controller/admin/form.config.php:241
+    'Title:' => '',
+
+    #: config/controller/admin/form.config.php:242
+    'Ms
+Mr' => '',
+
+    #: config/controller/admin/form.config.php:250
+    'Name:' => '',
+
+    #: config/controller/admin/form.config.php:257
+    'Address' => '',
+
+    #: config/controller/admin/form.config.php:263
+    'First address line:' => '',
+
+    #: config/controller/admin/form.config.php:267
+    'Second address line:' => '',
+
+    #: config/controller/admin/form.config.php:271
+    'Postal code:' => '',
+
+    #: config/controller/admin/form.config.php:275
+    'City:' => '',
+
+    #: config/controller/admin/form.config.php:282
+    #: config/controller/admin/form.config.php:398
+    'Message' => '',
+
+    #: config/controller/admin/form.config.php:288
+    #: config/controller/admin/form.config.php:420
+    'Message:' => '',
+
+    #: config/controller/admin/form.config.php:295
+    #: config/controller/admin/form.config.php:300
+    #: config/controller/admin/form.config.php:400
+    'Separator' => '',
+
+    #: config/controller/admin/form.config.php:308
+    #: config/controller/admin/form.config.php:399
+    'Hidden' => '',
+
+    #: config/controller/admin/form.config.php:315
+    'I’m the label for internal use only as I won’t be shown to users:' => '',
+
+    #: config/controller/admin/form.config.php:322
+    #: config/controller/admin/form.config.php:401
+    'Variable' => '',
+
+    #: config/controller/admin/form.config.php:342
+    'Properties' => '',
+
+    #: config/controller/admin/form.config.php:355
+    'Further options' => '',
+
+    #: config/controller/admin/form.config.php:366
+    'Technical options' => '',
+
+    #: config/controller/admin/form.config.php:385
+    'Type:' => '',
+
+    #: config/controller/admin/form.config.php:410
+    'Label:' => '',
+
+    #: config/controller/admin/form.config.php:413
+    'I’m the label, click to edit me:' => '',
+
+    #: config/controller/admin/form.config.php:424
+    'I’m a message, click to edit me.' => '',
+
+    #: config/controller/admin/form.config.php:431
+    'Style:' => '',
+
+    #: config/controller/admin/form.config.php:435
+    'Standard' => '',
+
+    #: config/controller/admin/form.config.php:436
+    'Heading 1' => '',
+
+    #: config/controller/admin/form.config.php:437
+    'Heading 2' => '',
+
+    #: config/controller/admin/form.config.php:438
+    'Heading 3' => '',
+
+    #: config/controller/admin/form.config.php:444
+    'Answers:' => '',
+
+    #. Note to translator: This is a placeholder, i.e. a field’s label shown within the field
+    #: config/controller/admin/form.config.php:450
+    'One answer per line' => '',
+
+    #: config/controller/admin/form.config.php:454
+    'Mandatory' => '',
+
+    #: config/controller/admin/form.config.php:462
+    'Default value:' => '',
+
+    #: config/controller/admin/form.config.php:469
+    'Instructions for the user:' => '',
+
+    #: config/controller/admin/form.config.php:476
+    'Width:' => '',
+
+    #: config/controller/admin/form.config.php:477
+    'Width: {{count}} characters' => '',
+
+    #: config/controller/admin/form.config.php:489
+    'Height: {{count}} lines' => '',
+
+    #: config/controller/admin/form.config.php:501
+    'Limited to {{count}} characters' => '',
+
+    #: config/controller/admin/form.config.php:511
+    'Origin:' => '',
+
+    #: config/controller/admin/form.config.php:524
+    'Variable name:' => '',
+
+    #: config/controller/admin/form.config.php:531
+    'Virtual field name:' => '',
+
+    #: config/controller/admin/form.config.php:539
+    'ID:' => '',
+
+    #: config/controller/admin/form.config.php:546
+    'CSS classes:' => '',
+
+    #: config/controller/admin/answer/inspector/date.config.php:18
+    'Answer date' => '',
+
+    #: config/controller/admin/answer/appdesk.config.php:25
+    'Received on:' => '',
+
+    #. Note to translator: 'Preview' here is a label, not an action
+    #: config/controller/admin/answer/appdesk.config.php:31
+    'Preview' => '',
+
+    #: config/controller/admin/answer/appdesk.config.php:32
+    'Click on an answer to preview it.' => '',
+
+    #: config/controller/admin/answer/appdesk.config.php:39
+    'answer' => '',
+
+    #: config/controller/admin/answer/appdesk.config.php:40
+    'answers' => '',
+
+    #: config/controller/admin/answer/appdesk.config.php:42
+    #: config/common/answer.config.php:57
+    '1 answer' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/answer/appdesk.config.php:46
+    'Showing 1 answer out of {{y}}' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/answer/appdesk.config.php:49
+    'No answers' => '',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/answer/appdesk.config.php:51
+    'Show all answers' => '',
+
+    #: config/controller/admin/appdesk.config.php:17
+    'form' => '',
+
+    #: config/controller/admin/appdesk.config.php:18
+    'forms' => '',
+
+    #: config/controller/admin/appdesk.config.php:20
+    #: config/common/form.config.php:76
+    '1 form' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:24
+    'Showing 1 form out of {{y}}' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/controller/admin/appdesk.config.php:27
+    'No forms' => '',
+
+    #. Note to translator: This is the action that clears the 'Search' field
+    #: config/controller/admin/appdesk.config.php:29
+    'Show all forms' => '',
+
+    #: config/common/form.config.php:28
+    #: config/common/form.config.php:111
+    'Answers' => '',
+
+    #. Crud
+    #: config/common/form.config.php:57
+    'All good! Your new form has been added.' => '',
+
+    #: config/common/form.config.php:58
+    'The form has been deleted.' => '',
+
+    #. General errors
+    #: config/common/form.config.php:61
+    'This form doesn’t exist any more. It has been deleted.' => '',
+
+    #: config/common/form.config.php:62
+    'We cannot find this form.' => '',
+
+    #. Deletion popup
+    #: config/common/form.config.php:65
+    'Deleting the form ‘{{title}}’' => '',
+
+    #: config/common/form.config.php:69
+    'Yes, delete this form' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/common/form.config.php:73
+    'We cannot delete this form as the number of answers you’ve entered is wrong. Please amend it.' => '',
+
+    #: config/common/form.config.php:121
+    #: classes/controller/admin/answer/appdesk.ctrl.php:30
+    'Answers to ‘{{title}}’' => '',
+
+    #: config/common/form.config.php:135
+    #: config/common/form.config.php:159
+    'There is no answers yet.' => '',
+
+    #: config/common/form.config.php:141
+    'Export the answers (spreadsheet)' => '',
+
+    #: config/common/answer.config.php:23
+    'Received on' => '',
+
+    #. Crud
+    #: config/common/answer.config.php:41
+    'The answer has been deleted.' => '',
+
+    #. General errors
+    #: config/common/answer.config.php:44
+    'This answer doesn’t exist any more. It has been deleted.' => '',
+
+    #: config/common/answer.config.php:45
+    'We cannot find this answer.' => '',
+
+    #. Deletion popup
+    #: config/common/answer.config.php:48
+    'Deleting the answer ‘{{title}}’' => '',
+
+    #: config/common/answer.config.php:52
+    'Yes, delete this answer' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: config/common/answer.config.php:66
+    'Visualise' => '',
+
+    #: config/common/answer.config.php:76
+    'Answer to ’{{title}}’' => '',
+
+    #: config/permissions.config.php:11
+    'Permissions for this application' => '',
+
+    #: config/model/answer.config.php:13
+    'Answer received on {{date}}' => '',
+
+    #: views/captcha.view.php:22
+    'Help us prevent spam: How much is {{number_1}} plus {{number_2}}?' => '',
+
+    #: views/captcha.view.php:35
+    'You have not passed the spam test. Please try again.' => '',
+
+    #: views/admin/warning_answers_collected.view.php:26
+    'Modifying the form may alter the collected data.' => '',
+
+    #: views/admin/warning_answers_collected.view.php:29
+    'You’re right, take me to the answers' => '',
+
+    #: views/admin/warning_answers_collected.view.php:31
+    'or' => '',
+
+    #: views/admin/warning_answers_collected.view.php:31
+    'Don’t worry, I know what I’m doing' => '',
+
+    #: views/admin/warning_answers_collected.view.php:49
+    'Answers to this form have already been received' => '',
+
+    #: views/admin/popup_delete.view.php:24
+    'This form has already received <strong>one answer</strong>.' => array(
+        0 => '',
+        1 => '',
+    ),
+
+    #: views/admin/popup_delete.view.php:33
+    'Yes, I want to delete this form and the {{count}} answers received.' => '',
+
+    #: views/admin/warning_not_published.view.php:18
+    'Not published' => '',
+
+    #: views/admin/warning_not_published.view.php:19
+    'To publish this form, add it to a page, a blog post or any other text editor.' => '',
+
+    #: views/admin/warning_not_published.view.php:22
+    '<p>Most Novius OS text editors (also called WYSIWYG editors) feature this button {{preview}} in the toolbar.</p><p>Click it to access the list of applications you can enhance your text with.</p>' => '',
+
+    #: views/admin/warning_not_published.view.php:23
+    'Applications' => '',
+
+    #: views/admin/permissions.view.php:15
+    'Can add, edit and delete forms and answers' => '',
+
+    #: views/admin/permissions.view.php:22
+    'Can visualise answers only' => '',
+
+    #: views/admin/layout_fields.view.php:28
+    'You have a problem here: Your Novius OS is not set up to send emails. You’ll have to ask your developer to set it up for you.' => '',
+
+    #: views/admin/layout_fields.view.php:47
+    #: views/admin/layout_fields.view.php:92
+    'Add a field' => '',
+
+    #: views/admin/layout_fields.view.php:53
+    'Standard fields' => '',
+
+    #: views/admin/layout_fields.view.php:54
+    'Special fields' => '',
+
+    #: views/admin/layout_fields.view.php:93
+    'Add a page break' => '',
+
+    #: views/admin/layout_fields.view.php:96
+    'Help us prevent spam: How much is 3 plus 8?' => '',
+
+    #: views/admin/layout_fields.view.php:103
+    'Delete' => '',
+
+    #: views/admin/layout_fields.view.php:132
+    'Form submission' => '',
+
+    #: views/admin/layout_fields.view.php:172
+    'Are you sure?' => '',
+
+    #: views/admin/answer.view.php:15
+    'Cancel' => '',
+
+    #: views/admin/answer.view.php:25
+    #: classes/controller/admin/answer.ctrl.php:33
+    'Answer to ‘{{title}}’' => '',
+
+    #: views/admin/answer.view.php:26
+    'Received on {{date}}' => '',
+
+    #: views/admin/answer.view.php:33
+    'Question' => '',
+
+    #: views/admin/answer.view.php:34
+    'Answer' => '',
+
+    #: views/admin/answer.view.php:55
+    'Page {{number}}' => '',
+
+    #: views/admin/answer.view.php:88
+    #: views/admin/answer.view.php:98
+    'Add to Media Centre' => '',
+
+    #: views/email.view.php:31
+    'Message sent by:' => '',
+
+    #: classes/controller/admin/form.ctrl.php:187
+    #: classes/controller/admin/form.ctrl.php:202
+    'Page break' => '',
+
+    #: classes/controller/admin/answer.ctrl.php:110
+    'No file attached.' => '',
+
+    #: classes/controller/admin/answer/appdesk.ctrl.php:36
+    'Export (spreadsheet)' => '',
+
+);
---
+++ lang/pt_BR/front.lang.php
@@ -0,0 +1,45 @@
+<?php
+
+// Generated on 07/07/2014 14:44:21
+
+// 12 out of 12 messages are translated (100%).
+// 66 out of 66 words are translated (100%).
+
+return array(
+    #: views/foundation.view.php:149
+    '{{previous}}{{pagination}}{{next}}' => '',
+
+    #: views/foundation.view.php:158
+    'Previous page' => '',
+
+    #: views/foundation.view.php:162
+    'Next page' => '',
+
+    #: views/foundation.view.php:169
+    '{{current}} out of {{total}}' => '',
+
+    #: classes/controller/front.ctrl.php:83
+    'Thank you. Your answer has been sent.' => '',
+
+    #: classes/controller/front.ctrl.php:444
+    '{{label}}: Please select a file for this field.' => '',
+
+    #: classes/controller/front.ctrl.php:470
+    '{{label}}: Please enter a value for this field.' => '',
+
+    #: classes/controller/front.ctrl.php:474
+    '{{label}}: ‘{{value}}’ is not a valid email.' => '',
+
+    #: classes/controller/front.ctrl.php:477
+    '{{label}}: ‘{{value}}’ is not a valid number.' => '',
+
+    #: classes/controller/front.ctrl.php:485
+    '{{label}}: ‘{{value}}’ is not a valid date.' => '',
+
+    #: classes/controller/front.ctrl.php:512
+    'You have not passed the spam test. Please try again.' => '',
+
+    #: classes/controller/front.ctrl.php:587
+    '{{form}}: New answer' => '',
+
+);
