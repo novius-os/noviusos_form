68d3d7646f3d6cdb3655fc3a8522f51d419923bb
Tony BOTALLA <botalla@novius.fr>
2017-01-19 11:13:10 +0100

    Move config JS

--- config/controller/front.config.php
+++ config/controller/front.config.php
@@ -1,7 +1,6 @@
 <?php
 
 return array(
-
     // The available layouts (you can configure which one to use in config/config.php)
     'layouts' => array(
         // Default layout (works with no framework)
@@ -10,10 +9,6 @@ return array(
             'view' => 'noviusos_form::front/form',
             // The view params
             'view_params' => array(
-                // The JS params injected before other scripts in "noviusos_form_params" JS global variable
-                'js_params_injected_inline' => array(
-                    'uri_base' => \Fuel\Core\Uri::base(),
-                ),
                 // The form script (for wizard, condition, etc...)
                 'script_url' => 'static/apps/noviusos_form/dist/js/front/form.min.js',
                 // The form stylesheet
@@ -29,9 +24,6 @@ return array(
         'foundation' => array(
             'view' => 'noviusos_form::front/form',
             'view_params' => array(
-                'js_params_injected_inline' => array(
-                    'uri_base' => \Fuel\Core\Uri::base(),
-                ),
                 'script_url' => 'static/apps/noviusos_form/dist/js/front/form.min.js',
                 'stylesheet_url' => 'static/apps/noviusos_form/dist/css/front/form-foundation.min.css',
                 'view_fields' => 'noviusos_form::front/form/fields-foundation',
@@ -43,14 +35,15 @@ return array(
         'bootstrap' => array(
             'view' => 'noviusos_form::front/form',
             'view_params' => array(
-                'js_params_injected_inline' => array(
-                    'uri_base' => \Fuel\Core\Uri::base(),
-                ),
                 'script_url' => 'static/apps/noviusos_form/dist/js/front/form.min.js',
                 'stylesheet_url' => 'static/apps/noviusos_form/dist/css/front/form-bootstrap.min.css',
                 'view_controls' => 'noviusos_form::front/form/controls-bootstrap',
                 'view_fields' => 'noviusos_form::front/form/fields-bootstrap',
             ),
         ),
+    ),
+    // The JS params injected before other scripts in "noviusos_form_params" JS global variable
+    'js_params_injected_inline' => array(
+        'uri_base' => \Fuel\Core\Uri::base(),
     ),
 );
