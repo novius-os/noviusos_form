17ca4971c1a85e52aabb4d4e7db9eefe894261f9
Tony BOTALLA <botalla@novius.fr>
2017-06-20 10:00:17 +0200

    Files' headers

--- migrations/004_add_conditional.php
+++ migrations/004_add_conditional.php
@@ -2,7 +2,7 @@
 /**
  * NOVIUS OS - Web OS for digital communication
  *
- * @copyright  2011 Novius
+ * @copyright  2017 Novius
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
  * @link http://www.novius-os.org
