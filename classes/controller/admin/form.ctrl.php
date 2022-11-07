a2bc076b2d671cd7b875f333696c0e120b36e7a6
Alexandre <ar.community@devnco.fr>
2022-11-07 16:35:21 +0100

    Update form.ctrl.php
    Trigger function before_export after set_time_limit

--- classes/controller/admin/form.ctrl.php
+++ classes/controller/admin/form.ctrl.php
@@ -332,9 +332,9 @@ class Controller_Admin_Form extends \Nos\Controller_Admin_Crud
      */
     public function action_export($id)
     {
-        \Event::trigger_function('noviusos_form::before_export', array($id));
-
         set_time_limit(5 * 60);
+        
+        \Event::trigger_function('noviusos_form::before_export', array($id));
 
         try {
             $helper = new Helper_Export();
